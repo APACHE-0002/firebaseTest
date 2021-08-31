596dfb28e1400f9d9f73f0b06a527f6876f3c1f0
APACHE-0002 <48190578+APACHE-0002@users.noreply.github.com>
2021-08-31 19:12:26 -0300

    envsurance

--- src/App.tsx
+++ src/App.tsx
@@ -17,14 +17,14 @@ export default class App extends React.Component<{}, {token: any}>{
     if (!firebase.apps.length) {
  
     let firebaseConfig = {
-      apiKey: "AIzaSyDzrxsXjts_sI-XHA4S2Qx2CpmWPKkXp0I",
-      authDomain: "reactapp-9371a.firebaseapp.com",
-      databaseURL: "https://reactapp-9371a.firebaseio.com",
-      projectId: "reactapp-9371a",
-      storageBucket: "reactapp-9371a.appspot.com",
-      messagingSenderId: "838921412121",
-      appId: "1:838921412121:web:dfd8fd2e88622d928d1488",
-      measurementId: "G-HHVZXD6ZEZ"
+      apiKey: "",
+      authDomain: "",
+      databaseURL: "",
+      projectId: "",
+      storageBucket: "",
+      messagingSenderId: "",
+      appId: "",
+      measurementId: ""
     };
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
